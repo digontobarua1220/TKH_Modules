546e49b37a1e99d8ea6f7b1e56f1e2dcfb97ad93
Digonto Barua <digontobarua1220@gmail.com>
2021-08-06 11:59:46 -0400

    pushing to module5 branch

---
+++ Module_5_Programming1/week5pretraining.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  6 11:33:35 2021
+
+@author: digon
+"""
+
+#1
+print("Hello World")
+
+#2
+input()
+
+#3
+a = 5
+if a == 5:
+    print("a equals 5")
+
+#4
+myString = 'hello world'
+print(type(myString))
+
+#5
+n = 10
+print(type(n))
+
+#7
+n_float = 7.5
+print(type(n_float))
+
+#11
+single_string = 'Its a single quote string'
+print(single_string)
+
+#13
+double_string = "It's a double quote string"
+print(double_string)
+
+#14
+n1 = 5
+n2 = 7.5
+print(n1+n2)
+
+#18
+nstr1 = "abc"
+print(nstr1 + str(n1))
+
+#19
+nstr2 = "def"
+print(nstr1 + nstr2)
+
+#23
+n1, n2 = 10, 11
+
+#24
+n1
+
+#25
+n2
+
+#27
+result = 3 + 4.0 / 2 * 5
+print(result)
+
+#28
+remainder = 17%10
+print(remainder)
+
+#29
+x = 5
+n = 4
+print(x ** n)
+
+#31
+nstr = "abc"
+ans = nstr * 10
+
+#32
+print(len(ans))
+
+#39
+name = "Harshit"
+print("%s is a data scientist" %name)
+
+#40
+name.upper()
+
+#41
+name.lower()
+
+#42
+nstr = "it is a nice day today."
+nstr.capitalize()
+
+#44
+name.index('s')
+
+#45
+name[2:5]
+
+#46
+name[2:6:2]
+
+#47
+alist = [3,4,5]
+print(alist)
+
+#48
+alist = ['harshit', 2, 5.5]
+print(alist)
+
+#49
+alist.append(10)
+alist.append(15)
+print(alist)
+
+#50
+alist.pop()
+print(alist)
+
+#53
+alist[3]
+
+#54
+alist[1:]
+
+#55
+alist.append([1,2,3])
+print(alist)
+
+#56
+alist*2
+
+#57
+alist + alist
